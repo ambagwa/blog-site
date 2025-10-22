565810fb3ffccb9414661f15d70870cd29bc488a
ambagwa <ambagwaeugene@gmail.com>
2025-10-22 06:47:35 +0300

    Add database connection setup using Mongoose

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+// Connect to the DB
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server connected successfully");
+  } catch (error) {
+    console.error("Server connection failed: ", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
