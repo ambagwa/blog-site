2fd339f81f3d8bc35760a1359850f9fbc3868318
ambagwa <ambagwaeugene@gmail.com>
2025-10-28 15:52:19 +0300

    Refactor App component to include Signin and Dashboard routes with ProtectedRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-
+import { Toaster } from "./components/ui/sonner";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
+import { Signin } from "./components/Signin"
 
 const App = () => {
-  
   return (
-   <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<LandingPage />} />
-     </Routes>
-   </BrowserRouter> 
-  )
-}
+    <BrowserRouter>
+    <Toaster position="top-center"/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Signin />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
