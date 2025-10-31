a8919758f1160589002dfee8b31535d8c2eb2523
ambagwa <ambagwaeugene@gmail.com>
2025-10-31 15:47:08 +0300

    Refactor Navbar component for improved styling and button sizes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,20 +25,21 @@ export const Navbar = () => {
   const isAdmin = userRole === "admin";
 
   return (
-    <nav className="bg-white dark:bg-gray-800 sticky top-0 z-50 border-b md:px-20 border-zinc-200 dark:border-zinc-700 px-4 py-2 flex items-center justify-between shadow-lg">
-      <div className="flex items-center space-x-1">
-        <BookOpenIcon className="size-6 text-blue-500" />
-        <Link to="/dashboard" className="font-bold text-lg">
+    <nav className="bg-white dark:bg-gray-800 sticky top-0 z-50 border-b md:px-20 border-zinc-200 dark:border-zinc-700 px-4 py-4 flex items-center justify-between shadow-lg">
+      <div className="flex items-center space-x-2">
+        <BookOpenIcon className="size-8 text-blue-500" />
+        <Link to="/dashboard" className="font-bold text-xl mb-1">
           BlogHub
         </Link>
       </div>
 
       <div className="flex items-center gap-1 justify-end">
+        <div className="flex">
         {/**Explore button - Only shown to admin */}
         {isAdmin && (
           <Link to="/dashboard/explore">
             <Button
-              size="xs"
+              size="md"
               className={`hover:cursor-pointer ${
                 active === "explore" ? "bg-blue-600 text-white" : ""
               }`}
@@ -53,7 +54,7 @@ export const Navbar = () => {
         {/**My Blogs button - show to all users */}
         <Link to="/dashboard/my-blogs">
           <Button
-            size="xs"
+            size="md"
             onClick={() => handleNavClick("myblogs")}
             variant={active === "myblogs" ? "blue" : "ghost"}
             className={`hover:cursor-pointer flex items-center gap-1 ${
@@ -64,19 +65,21 @@ export const Navbar = () => {
             My Blogs
           </Button>
         </Link>
+        </div>
 
         <ThemeToggle />
         <div className="flex items-center gap-3">
           <span className="text-sm text-gray-600 md:ml-5 bg-blue-100 py-1 px-2 rounded-4xl">
             {isAdmin ? "Admin" : username }
           </span>
-          <button
+          <Button
+          variant="ghost"
             onClick={logout}
-            className="flex items-center text-gray-700 dark:text-gray-100 dark:hover:text-gray-500 hover:text-red-600 transition-colors hover:cursor-pointer"
+            className="flex items-center  dark:text-gray-500  hover:text-red-600 dark:hover:text-red-600 transition-colors hover:cursor-pointer"
             title="Log Out"
           >
             <LogOut size={12} />
-          </button>
+          </Button>
         </div>
       </div>
     </nav>
